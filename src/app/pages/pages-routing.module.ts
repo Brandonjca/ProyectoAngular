2f5c735cc19e3cd8bfbedf1fef68250662c5918c
Brandonjca <brandito144@gmail.com>
2023-01-11 22:05:11 -0500

    Product

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -8,20 +8,21 @@ import { ProductComponent } from './product/product.component';
 
 const routes: Routes = [
   //rutas protegidas
-  {path: '', component:PagesComponent,
-   children:[
+    {path: '', component:PagesComponent},
     {path: 'dashboard', component:DashboardComponent},
     {path: 'category', component:CategoryComponent},
     {path: 'product', component:ProductComponent},
-] },
+]
   //{path: '**', component:NotFoundComponent},
-
-];
+;
 
 @NgModule({
   declarations: [],
   imports: [
     RouterModule.forChild(routes),
-  ]
+  ],
+  exports:[
+    RouterModule
+  ],
 })
 export class PagesRoutingModule { }
--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient as HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
@@ -6,5 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./product.component.css']
 })
 export class ProductComponent {
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void{
+    //this.getProducts();
+    //this.getProduct();
+    this.createProduct();
+    //this.updateProduct();
+  }
+
+  getProducts():void{
+    const url ="https://api.escuelajs.co/api/v1/products";
+    const response = this.httpClient.get(url).subscribe
+    (response => {console.log(response);
+    });
+  }
+
+  getProduct(){
+    const url ="https://api.escuelajs.co/api/v1/products/20";
+    const response = this.httpClient.get(url).subscribe
+    (response => {console.log(response);
+    });
+  }
+
+  createProduct(){
+    const data = {
+      title:"Zapatos",
+      price:20,
+      description:"deportivos / Brandon Caranqui",
+      images:["https://sdfsdgsdhasjh"],
+      categoryId:1
+    }
+    const url = "https://api.escuelajs.co/api/v1/products";
+    this.httpClient.post(url, data).subscribe(
+      response => {console.log(response);
+      }
+    );
+  }
+
+  updateProduct(){
+    const data = {
+      title:"camisas",
+      price:25,
+      description:"ropa deportiva / Brandon Caranqui",
+      images:["https://sdfsdgsdhasjh"],
+      categoryId:1
+    }
+    const url = "https://api.escuelajs.co/api/v1/products/226";
+    this.httpClient.put(url, data).subscribe(
+      response => {console.log(response);
+      }
+    );
+  }
 
 }
